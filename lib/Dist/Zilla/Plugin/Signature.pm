7dd7657fec52e42f5badb2d14d9b84e36b34afd2
Graham Barr <gbarr@pobox.com>
2010-03-31 14:20:43 -0500

    Do not require Dist::Zilla::Role::BeforeArchive as it is not part of Dist::Zilla

--- lib/Dist/Zilla/Plugin/Signature.pm
+++ lib/Dist/Zilla/Plugin/Signature.pm
@@ -1,17 +1,38 @@
 package Dist::Zilla::Plugin::Signature;
 use Moose;
 with 'Dist::Zilla::Role::FileGatherer';
-with 'Dist::Zilla::Role::BeforeArchive';
+with 'Dist::Zilla::Role::AfterBuild';
 
 
-sub before_archive {
+has sign => (is => 'ro', lazy_build => 1);
+
+sub _build_sign {
+  my $sign = 0;
+
+  my $i = 0;
+
+  # Not the best way to determine if an archive is being built
+  # but there is not hook for BeforeArchive
+
+  while (my ($package, $filename, $line, $subroutine) = caller($i++)) {
+    if ($subroutine eq 'Dist::Zilla::build_archive') {
+      $sign = 1;
+      last;
+    }
+  }
+  return $sign;
+}
+
+sub after_build {
   my ($self, $arg) = @_;
 
   require Module::Signature;
   require File::chdir;
 
-  local $File::chdir::CWD = $arg->{build_root};
-  Module::Signature::sign(overwrite => 1) && die "Cannot sign";
+  if (exists $ENV{DZSIGN} ? $ENV{DZSIGN} : $self->sign) {
+    local $File::chdir::CWD = $arg->{build_root};
+    Module::Signature::sign(overwrite => 1) && die "Cannot sign";
+  }
 }
 
 
@@ -45,7 +66,24 @@ Dist::Zilla::Plugin::Signature - sign releases with Module::Signature
 
 =head1 DESCRIPTION
 
-This plugin will sign a distribution using Module::Signature
+This plugin will sign a distribution using Module::Signature.
+
+This plugin should appear after any other AfterBuild plugin in your C<dist.ini> file
+to ensre that no files are modified after it has been run
+
+=head1 ATTRIBUTES
+
+=over
+
+=item sign
+
+A boolean value that if true will cause the build directory to get signed every time it it built.
+By default the directory is only signed when an archive is to be built from it. If this attibute
+is set false, then the directory will not be signed.
+
+This attribute can be overridden by an environment variable C<DZSIGN>
+
+=back
 
 =head1 AUTHOR
 
